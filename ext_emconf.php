8beca4589fc6bde7617da923cd3d6b219d4662e1
Guido Ilse <gi@webticity.de>
2022-06-17 10:33:31 +0200

    private composer update fpr t11

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,7 +23,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '10.1.0-10.4.99',
+			'typo3' => '10.1.0-10.5.99',
 			'extbase' => ''		),
 		'conflicts' => array(
 		),
